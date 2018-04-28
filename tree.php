9b170c0990c3ccc4bf598a739f8ef9f1adc40335
Eliana Grosof <elig121@gmail.com>
2018-04-28 10:39:29 -0400

    message

--- tree.php
+++ tree.php
@@ -1,13 +1,12 @@
 <?php
 // Gets data from URL parameters.
-$treeID = $_GET['treeID'];
 $type = $_GET['type'];
 $height = $_GET['height'];
 $diff = $_GET['difficulty'];
 $lat = $_GET['lat'];
 $lng = $_GET['lng'];
 
-
+alert($type);
 // Opens a connection to a MySQL server.
 
 $connection=mysqli_connect("127.0.0.1:3306", "root", "timbrcity", "mydb");
@@ -16,26 +15,29 @@ if (!$connection){
   exit;
   }
   else {
-    echo "Connected!";
+    echo "Connected!\n";
   }
-//echo "Success: A proper connection to MySQL was made! The id5405142_treeinfo database is great." . PHP_EOL;
-//echo "Host information: " . mysqli_get_host_info($connection) . PHP_EOL;
+
+echo "Host information: " . mysqli_get_host_info($connection) . PHP_EOL;
 
 // Sets the active MySQL database.
-$db_selected = mysqli_select_db($connection, id5405142_treeinfo);
+$db_selected = mysqli_select_db($connection, "mydb");
 if (!$db_selected) {
   die ('Can\'t use db : ' . mysql_error());
 }
 
 // Inserts new row with place data.
-$sql = "INSERT INTO `Trees` (`treeID`, `type`, `height`, `difficulty`, `lat`, `lng`)
-VALUES (`1234`, `red`, `4`, `1`, `001.111`, `002.2222`)"; //($treeID, $type, $height, $diff, $lat, $lng)";
+$sql = "INSERT INTO Tree (treeID, latitude, longitude, type, height, difficulty)
+VALUES (DEFAULT, '41', '42', 'dec', 'five foot', '5')";
 
-echo "Conn" . $connection->query($sql) === TRUE;
-if ($connection->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: ";
+// "(DEFAULT,'$lat', '$lat', '$type', '$height', '$diff')";
+
+
+if(mysqli_query($connection, $sql
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not execute $sql. \n" . mysqli_error($connection);
 }
-//error stuff doesn't work rn
+
+
 ?>
